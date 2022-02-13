c93f06aaa61a2f52ae7cac15ba2b63aee7c327dd
SumitK27 <sumitkajbaje@gmail.com>
2022-02-13 17:17:51 +0530

    Added /ping

--- index.ts
+++ index.ts
@@ -7,12 +7,43 @@ const client = new DiscordJS.Client({
 
 client.on("ready", () => {
     console.log("The bot is ready");
+
+    // Slash Command (Guild: only for single server)
+    const guildId = "715185011229327410";
+    const guild = client.guilds.cache.get(guildId);
+    let commands;
+
+    if (guild) {
+        // Add Commands to server
+        commands = guild.commands;
+    } else {
+        // Add Commands to the bot (for all the servers)
+        commands = client.application?.commands;
+    }
+
+    // Create a 'ping' command
+    commands?.create({
+        name: "ping",
+        description: "replies with pong.",
+    });
 });
 
-client.on("messageCreate", (message) => {
-    if (message.content === "ping") {
-        message.reply({
-            content: "Pong!",
+// Reply to the message
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+
+    const { commandName, options } = interaction;
+
+    if (commandName === "ping") {
+        // Send Reply
+        interaction.reply({
+            // Message to reply
+            content: "pong",
+
+            // Only user sent the command can see
+            ephemeral: true,
         });
     }
 });
