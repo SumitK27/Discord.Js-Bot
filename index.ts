7a7bd5eaaa6aa8a2af5e4df4faf1b3a2528ba840
SumitK27 <sumitkajbaje@gmail.com>
2022-02-13 17:39:04 +0530

    Added /add command

--- index.ts
+++ index.ts
@@ -26,6 +26,25 @@ client.on("ready", () => {
         name: "ping",
         description: "replies with pong.",
     });
+
+    commands?.create({
+        name: "add",
+        description: "Adds two numbers.",
+        options: [
+            {
+                name: "num1",
+                description: "The first number",
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            },
+            {
+                name: "num2",
+                description: "The second number",
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            },
+        ],
+    });
 });
 
 // Reply to the message
@@ -43,6 +62,24 @@ client.on("interactionCreate", async (interaction) => {
             content: "pong",
 
             // Only user sent the command can see
+            ephemeral: true,
+        });
+    } else if (commandName === "add") {
+        // Get values of options from the command
+        const num1 = options.getNumber("num1")!;
+        const num2 = options.getNumber("num2")!;
+
+        // Allow bot to take more than default time (3s) to reply
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+
+        // Delay reply 5s (testing)
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+
+        // Reply
+        interaction.reply({
+            content: `The sum is ${num1 + num2}`,
             ephemeral: true,
         });
     }
